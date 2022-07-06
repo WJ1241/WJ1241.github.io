1ae46f9547dd4737a3df61af05e66b2a38e6464d
WJ1241 <83521037+WJ1241@users.noreply.github.com>
2022-07-06 16:22:08 +0100

    06/07/22 16:21 - Added CV - Added Thumbnail Images regarding portfolio - Edited custom.js - Edited index.html - Edited style.css

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -135,9 +135,9 @@
 		    var target = this.hash,
 		    $target = $(target);
 		    $('html, body').stop().animate({
-		        'scrollTop': $target.offset().top
+		        'scrollTop': $target.offset().top - 30
 		    }, 1000, 'swing', function () {
-		        window.location.hash = target;
+		        window.location.hash = target - 30;
 			});
 		});
 
